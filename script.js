b7e4bb000a389bdfc82bfe64ed6230673b86faba
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-01-05 17:24:30 -0500

    meme func

---
+++ script.js
@@ -0,0 +1,42 @@
+function generateMeme(img, topText, bottomText, topTextSize, bottomTextSize) {
+    const canvas = document.getElementById('meme-canvas');
+    const ctx = canvas.getContext('2d');
+
+    // Size canvas to image
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Draw main image
+    ctx.drawImage(img, 0, 0);
+
+    // Text style: white with black borders
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.textAlign = 'center';
+
+    // Top text font size
+    let fontSize = canvas.width * topTextSize;
+    ctx.font = `${fontSize}px Impact`;
+    ctx.lineWidth = fontSize / 20;
+
+    // Draw top text
+    ctx.textBaseline = 'top';
+    topText.split('\n').forEach((t, i) => {
+        ctx.fillText(t, canvas.width / 2, i * fontSize, canvas.width);
+        ctx.strokeText(t, canvas.width / 2, i * fontSize, canvas.width);
+    });
+
+    // Bottom text font size
+    fontSize = canvas.width * bottomTextSize;
+    ctx.font = `${fontSize}px Impact`;
+    ctx.lineWidth = fontSize / 20;
+
+    // Draw bottom text
+    ctx.textBaseline = 'bottom';
+    bottomText.split('\n').reverse().forEach((t, i) => { // .reverse() because it's drawing the bottom text from the bottom up
+        ctx.fillText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
+        ctx.strokeText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
+    });
+}
